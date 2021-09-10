043a8855a2297b87b1cc91d23fc6f85db2c6785e
nitishhurduke <nitishhurduke@gmail.com>
2021-09-10 08:45:54 +0530

    two common letters bug fixed

--- Abc.java
+++ Abc.java
@@ -2,33 +2,61 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		String[] a1 = {"abc","efg","zxc"};
-		String[] b1 = {"abf","ade","urv"};
+		String[] a1 = {"abc","efg","zxc", "aaa"};
+		String[] b1 = {"abf","ade","urv", "aaa"};
 	    	int[]  count = new int[a1.length];
-	    	String[] common = new String[a1.length]
-		int k = 0;
-		boolean b = false;
+	    	String[] common = new String[a1.length];
+		
+		int l = 0;
 
 		System.out.println("a1  b1 common bool");
-	        for(int h = 0; h<a1.length; h++)
+	        for(int h = 0; h<a1.length; h++)//All strings in a1 and b1 array 
 	        {
-		    for (int i = 0;i<3;i++)
-		    {	
-		    	for(int j = 0; j<3;j++)
-		    	{
-		    		if(a1[h].charAt(i) == b1[h].charAt(j))
-		    		{	
-										    
-                            		System.out.println(a1[h]+" "+b1[h]+" "+a1[h].charAt(i)+ "   Yes"); 
-		               		count[h]++;						           
-		    	        }
+	        	common[h] = "";
+	        	char[] com = common[h].toCharArray();
+		        boolean bool = false;
+			
+			for (int i = 0;i<3;i++)//All letters from all Strings in a1 Array 
+			{	
+		    		for(int j = 0; j<3;j++)//All letters from b1[h] String compared with each a1[h] letters separately
+		    		{
+		    			if(a1[h].charAt(i) == b1[h].charAt(j))
+		    			{										    
+                            			if(common[h].length()>1){
+		        				for(int q = 0 ;q<common[h].length();q++)
+		        				{
+		        					for(int p = 0;p<common[h].length();p++)
+		        					{
+		        						if(bool == false)
+		        						{
+		        							if(com[q] == com[p])
+		        							{	
+		        								common[h] += a1[h].charAt(i);
+		        								bool = true;
+		        								l++;
+		        							}
+		        						}
+		        					}
+		        				}
+		        			}else{
+                            				common[h] += a1[h].charAt(i);
+                            					
+		               		}
+		               		l++;                            			
+		               		count[h]++;					           
+		    	        	}
 			 		
-	      	    	}    	    		  	       
-	            }
-	    	    if(count[h] == 0) 
-		    {
-			System.out.println(a1[h]+" "+b1[h]+"     NO");
-		    }			
-	    }
+	      	    		}    	    		  	       
+	            	}	
+	    	  	 
+	    	  	if(count[h] == 0) 
+		        {
+				System.out.println(a1[h]+" "+b1[h]+"     NO");
+		        }else{
+		        	
+		        	System.out.println(a1[h]+" "+b1[h]+" "+common[h]+ "   Yes");
+		        }
+		        l = 0;	 		
+	         }
 	}    
 }
