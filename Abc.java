4d6d238d296d016d20b2e17308c8f56b10d40737
nitishhurduke <nitishhurduke@gmail.com>
2021-09-10 09:33:56 +0530

    Complete Solution

--- Abc.java
+++ Abc.java
@@ -13,40 +13,26 @@ public class Abc
 	        for(int h = 0; h<a1.length; h++)//All strings in a1 and b1 array 
 	        {
 	        	common[h] = "";
-	        	char[] com = common[h].toCharArray();
-		        boolean bool = false;
+	        	
+		        
 			
 			for (int i = 0;i<3;i++)//All letters from all Strings in a1 Array 
 			{	
-		    		for(int j = 0; j<3;j++)//All letters from b1[h] String compared with each a1[h] letters separately
-		    		{
-		    			if(a1[h].charAt(i) == b1[h].charAt(j))
-		    			{										    
-                            			if(common[h].length()>1){
-		        				for(int q = 0 ;q<common[h].length();q++)
-		        				{
-		        					for(int p = 0;p<common[h].length();p++)
-		        					{
-		        						if(bool == false)
-		        						{
-		        							if(com[q] == com[p])
-		        							{	
-		        								common[h] += a1[h].charAt(i);
-		        								bool = true;
-		        								l++;
-		        							}
-		        						}
-		        					}
-		        				}
-		        			}else{
-                            				common[h] += a1[h].charAt(i);
-                            					
-		               		}
-		               		l++;                            			
-		               		count[h]++;					           
+		    		aa:
+		    			for(int j = 0; j<3;j++)//All letters from b1[h] String compared with each a1[h] letters separately
+		    			{
+		    				if(a1[h].charAt(i) == b1[h].charAt(j))
+		    				{										    
+                            				common[h] += a1[h].charAt(i);        
+		               			l++;   		                               			
+		               			count[h]++;
+		               			break aa;
+		               							           
 		    	        	}
 			 		
 	      	    		}    	    		  	       
+	            		
+	            		
 	            	}	
 	    	  	 
 	    	  	if(count[h] == 0) 
